12c84f904863936465126bd3121caf3976327e41
Samuel <knightincode@gmail.com>
2026-04-16 22:04:48 +0000

    Use lowercase admin email in seed to match normalization

--- server/adminUser.ts
+++ server/adminUser.ts
@@ -2,7 +2,7 @@ import { db } from "./db";
 import { users } from "../shared/schema";
 import { sql } from "drizzle-orm";
 
-const ADMIN_EMAIL = "BeatHavenAdmin@gmail.com";
+const ADMIN_EMAIL = "beathavenadmin@gmail.com";
 const ADMIN_PASSWORD_HASH = "$2b$10$vhbx8ytH4if0eQ1//mGu7.2MC/ybl4HRIWey0Coe7w1wTB7SvG9gW";
 
 export async function seedAdminUser(): Promise<void> {
